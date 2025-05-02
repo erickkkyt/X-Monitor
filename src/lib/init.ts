8069cf18197ea6800602bc9cda42aaadf8b5a6f5
erickkkyt <kh844257437@gmail.com>
2025-05-03 03:26:39 +0800

    5.3-v29

--- src/lib/init.ts
+++ src/lib/init.ts
@@ -1,14 +1,9 @@
-import { startCronServices, stopCronServices } from './cron/service';
-
 /**
  * 初始化应用服务
  * 这个函数应该在应用启动时被调用
  */
 export function initializeServices() {
   console.log('正在初始化应用服务...');
-  
-  // 启动定时任务服务
-  startCronServices();
   
   // 注册应用关闭事件处理程序
   registerShutdownHandlers();
@@ -24,9 +19,6 @@ function registerShutdownHandlers() {
   // 处理进程终止信号
   const handleShutdown = () => {
     console.log('应用正在关闭...');
-    
-    // 停止所有定时任务服务
-    stopCronServices();
     
     console.log('应用已安全关闭');
     process.exit(0);
